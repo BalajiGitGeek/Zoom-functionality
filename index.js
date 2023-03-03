667653703b750ef38041da83027898bd17761e00
Balaji <125738570+BalajiGitGeek@users.noreply.github.com>
2023-03-03 09:08:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let dots = document.getElementsByClassName("demo");
+  if (n > dots.length) {slideIndex = dots.length}
+  if (n < 1) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  dots[slideIndex-1].className += " active";
+}
